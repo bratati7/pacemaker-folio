7c506f8e82f91dd51b7ae2913bc488915d849c9b
Sourashis Ghosh Roy <personal.sourashisghoshroy@gmail.com>
2025-05-04 10:46:26 +0530

    update

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Styles from "./footer.module.css";
 export default function Footer() {
   return (
     <nav className={Styles.navBar}>
-      <h2 className={Styles.brandName}>This is a Portfolio of Cardiac Pacemakers, built by Bratati Sen.</h2>
+      <h2 className={Styles.brandName}>This is a Portfolio of Cardiac Pacemakers, built by Bratati Sen, in collaboration.</h2>
     </nav>
   );
 }
